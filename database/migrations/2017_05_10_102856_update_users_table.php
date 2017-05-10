d23790a75e46ffaaad898d6cf37d0e2cf0369de5
JuliaLitovenko <oleg.shumar@gmail.com>
2017-05-10 13:30:31 +0300

    added columns to user table

---
+++ database/migrations/2017_05_10_102856_update_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->renameColumn('name', 'first_name');
+            $table->string('last_name');
+            $table->string('about_myself');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
+
